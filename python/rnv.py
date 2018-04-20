c65bf7f2072b5dee51d9fac708b5304c3f54b7ec
Arnold-Franck Mietchop-Touko <inf2613@hs-worms.de>
2018-04-21 00:44:18 +0200

    rnv

--- python/rnv.py
+++ python/rnv.py
@@ -2,5 +2,14 @@ import json
 
 #import requests internet Abfrage
 #response = requests.get('https://opendata.rnv-online.de/sites/default/files/Haltestellen_79.json')
-fh = open('../haltestellen.json', 'r') #locale abfrage
+fh = open('../datasets/haltestellen.json', 'r') #locale abfrage
 haltestellen = json.load(fh)
+
+fh = open('../datasets/Linien.json','r')
+Linien = json.load(fh)
+
+fh = open('../datasets/Linien_mit_Haltestellenreferenz.json','r')
+Linien_mit_Haltestellenreferenz = json.load(fh)
+
+fh = open('../datasets/Haltestellen_mit_Linienreferenz.json','r')
+Haltestellen_mit_Linienreferenz = json.load(fh)
