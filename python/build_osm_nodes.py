cd16f46c5b3ac0dc0012284f81075be5bc206447
Guangyang Wen <guangyang.wen@polytechnique.edu>
2018-04-21 09:38:04 +0200

    update all_nodes.json

--- python/build_osm_nodes.py
+++ python/build_osm_nodes.py
@@ -8,18 +8,32 @@ BBOX = (rnv.MIN_LAT - 0.05, rnv.MIN_LON - 0.05, rnv.MAX_LAT + 0.05,
         rnv.MAX_LON + 0.05)
 
 
-def main():
+def get_nodes(node_type):
     response = requests.post('http://overpass-api.de/api/interpreter', {
         'data':
         """[out:json]
         [bbox:{0},{1},{2},{3}];
-        (node[amenity];node[leisure];node[tourism];node[shop]);
-        out body;""".format(*BBOX)
+        node["{4}"];
+        out body;""".format(*BBOX, node_type)
     })
-    print(response.text)
     result = json.loads(response.text)
-    all_nodes = result['elements']
+    return result['elements']
 
+
+def main():
+    all_nodes = []
+    general_node_types = set()
+    for node_type in ('amenity', 'leisure', 'tourism', 'shop'):
+        nodes = get_nodes(node_type)
+        for n in nodes:
+            general_node_type = '{0}:{1}'.format(node_type,
+                                                 n['tags'][node_type])
+            n['type'] = general_node_type
+            general_node_types.add(general_node_type)
+        all_nodes += nodes
+
+    print(general_node_types)
+    print(len(general_node_types))
     with open('../datasets/all_nodes.json', 'w') as f:
         json.dump(all_nodes, f)
     print(len(all_nodes))
