5c07a116b49fafe2fc23be0442fcf14145c0df5a
Guangyang Wen <guangyang.wen@polytechnique.edu>
2018-04-21 12:01:50 +0200

    update voronoi

--- html/main.js
+++ html/main.js
@@ -1,4 +1,4 @@
-/* global L, axios, cmap */
+/* global L, axios, cmap, d3 */
 
 const MIN_ZOOM = 8;
 const MAX_ZOOM = 20;
@@ -14,6 +14,7 @@ const initMap = () => {
     zoom: 14,
     editable: true
   });
+  const icon = L.divIcon({ className: 'icon' });
 
   var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   var osm = new L.TileLayer(osmUrl, {
@@ -21,44 +22,78 @@ const initMap = () => {
     maxZoom: MAX_ZOOM
   });
   map.addLayer(osm);
-
   map.setView(new L.LatLng(INIT_LATITUDE, INIT_LONGITUDE));
 
-  const icon = L.divIcon({ className: 'icon' });
+  const stopPromise = axios.get('../datasets/haltestellen.json');
+  const allNodesPromise = axios.get('../datasets/all_nodes.json');
 
-  axios.get('../datasets/haltestellen.json').then(res => {
+  const stopsHandler = res => {
     const data = res.data;
     const stopLocations = [];
+    const stopLayers = [];
     for (const stopId in data) {
       if (!data.hasOwnProperty(stopId)) {
         return;
       }
       const { name, stops } = data[stopId];
-      stops.forEach(({ lat, lon }) =>
-        // stopLocations.push([lat, lon, 1])
-        L.marker(new L.LatLng(lat, lon), {
-          icon: icon,
-          keyboard: false,
-          title: name
-        }).addTo(map)
-      );
+      var avgLat = 0;
+      var avgLng = 0;
+      stops.forEach(({ lat, lon }) => {
+        avgLat += lat;
+        avgLng += lon;
+        stopLayers.push(
+          L.marker(new L.LatLng(lat, lon), {
+            icon: icon,
+            keyboard: false,
+            title: name
+          }).bindPopup(name)
+        );
+      });
+      stopLocations.push([avgLat / stops.length, avgLng / stops.length]);
     }
-  });
+    const stopGroup = L.layerGroup(stopLayers).addTo(map);
 
-  axios.get('../datasets/all_nodes.json').then(res => {
+    const voronoi = d3
+      .voronoi()
+      .extent([[49.34345 - 0.05, 8.170481 - 0.05], [49.581196 + 0.05, 8.811391 + 0.05]]);
+    const polygons = voronoi(stopLocations);
+    const voronoiLayers = polygons.edges
+      .filter(([p1, p2]) => p1 !== null && p2 !== null)
+      .map(([p1, p2]) => {
+        return L.polyline([p1, p2], { color: 'black', weight: 1 });
+      });
+    const voronoiGroup = L.layerGroup(voronoiLayers).addTo(map);
+
+    return [stopGroup, voronoiGroup];
+  };
+
+  const allNodesHandler = res => {
     const data = res.data;
-
     const restaurants = data
       .filter(({ tags }) => tags['amenity'] === 'restaurant')
       .map(({ lat, lon }) => [lat, lon, 1]);
 
-    const heat = L.heatLayer(restaurants, {
+    const restaurantHeatMap = L.heatLayer(restaurants, {
       radius: 50,
       max: 1,
       gradient: cmap,
       minOpacity: 0.5,
       blur: 25
     }).addTo(map);
+
+    return [restaurantHeatMap];
+  };
+
+  Promise.all([stopPromise, allNodesPromise]).then(([stopRes, allNodesRes]) => {
+    const [stopGroup, voronoiGroup] = stopsHandler(stopRes);
+    const [restaurantHeatMap] = allNodesHandler(allNodesRes);
+    L.control
+      .layers(null, {
+        Stops: stopGroup,
+        'Voronoi Boundaries': voronoiGroup,
+        'Heat Map': restaurantHeatMap
+      })
+      .addTo(map);
   });
 };
 
