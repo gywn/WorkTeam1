121b4e38ef9f62350474556caf39814432abf30e
Guangyang Wen <guangyang.wen@polytechnique.edu>
2018-04-21 07:45:56 +0200

    update rnv

--- python/rnv.py
+++ python/rnv.py
@@ -5,6 +5,19 @@ import json
 fh = open('../datasets/haltestellen.json', 'r') #locale abfrage
 haltestellen = json.load(fh)
 
+MIN_LAT = 90
+MAX_LAT = -90
+MIN_LON = 180
+MAX_LON = -180
+
+for name in haltestellen:
+    for stop in haltestellen[name]['stops']:
+        MIN_LAT = min(MIN_LAT, stop['lat'])
+        MAX_LAT = max(MAX_LAT, stop['lat'])
+        MIN_LON = min(MIN_LON, stop['lon'])
+        MAX_LON = max(MAX_LON, stop['lon'])
+
+
 fh = open('../datasets/Linien.json','r')
 Linien = json.load(fh)
 
@@ -13,3 +26,10 @@ Linien_mit_Haltestellenreferenz = json.load(fh)
 
 fh = open('../datasets/Haltestellen_mit_Linienreferenz.json','r')
 Haltestellen_mit_Linienreferenz = json.load(fh)
+
+
+if __name__ == '__main__':
+    print(MIN_LAT)
+    print(MAX_LAT)
+    print(MIN_LON)
+    print(MAX_LON)
